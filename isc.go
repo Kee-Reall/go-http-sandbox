0b0dfe07bb9a5ad10991938626e737236409a4f8
Kee-Reall <kirill_bezrodny@mail.ru>
2023-06-10 20:36:56 +0500

    gitignore

---
+++ isc.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	defer handlePanic()
+	var cmd string = "node"
+	is := checkCmd(cmd)
+	var ableStr string
+	if is {
+		ableStr = "avaliable"
+	} else {
+		ableStr = "not avaliable"
+	}
+	fmt.Printf("%s is %s", cmd, ableStr)
+}
+
+func checkCmd(command string) bool {
+	_, err := exec.LookPath(command)
+	if err != nil {
+		fmt.Printf("%v+", err.Error())
+	}
+	return err == nil
+}
+
+func handlePanic() {
+	if msg := recover(); msg != nil {
+		fmt.Printf("%v+", msg)
+	}
+}
